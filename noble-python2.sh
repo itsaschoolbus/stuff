f9e8577dfcef0095d4001c821ede07c7a6cf92b1
itsaschoolbus <itsaschoolbus@proton.me>
2025-08-10 15:08:11 +0700

    Update noble-python2.sh

--- noble-python2.sh
+++ noble-python2.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -e
 
-mkdir -p ~/python2-jammy && cd ~/python2-jammy
+if [ -w /tmp ]; then
+  DEBDIR="/tmp/python2-debs"
+else
+  DEBDIR="$(pwd)/python2-debs"
+fi
+mkdir -p "$DEBDIR"
+cd "$DEBDIR"
+
 wget http://security.ubuntu.com/ubuntu/pool/universe/p/python2.7/python2.7_2.7.18-13ubuntu1.5_amd64.deb
 wget http://security.ubuntu.com/ubuntu/pool/universe/p/python2.7/libpython2.7-stdlib_2.7.18-13ubuntu1.5_amd64.deb
 wget http://security.ubuntu.com/ubuntu/pool/universe/p/python2.7/python2.7-minimal_2.7.18-13ubuntu1.5_amd64.deb
